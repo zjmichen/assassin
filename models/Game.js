59676e6c0e540c1310885a9a0a794613a69f7ed9
Zack Michener <zack@zackmichener.net>
2014-12-18 19:35:41 +0000

    only allow adding player to game if game is pending

--- models/Game.js
+++ models/Game.js
@@ -2,12 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GameSchema = Schema({
+  state: { type: String, default: 'pending' },
   players: [{ type: Schema.Types.ObjectId, ref: 'Player' }]
 });
 
 GameSchema.method('addPlayer', function(player, done) {
-  this.players.push(player);
-  done(null);
+  if (this.state === 'pending') {
+    this.players.push(player);
+    done(null);
+  } else {
+    done('Game in progress');
+  }
 });
 
 mongoose.model('Game', GameSchema);
