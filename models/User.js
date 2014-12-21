4ffaab4aa96835f2569e5371fc01ab637bfd2703
Zack Michener <zack@zackmichener.net>
2014-12-21 19:35:58 +0000

    add User#findOrCreateByEmail

--- models/User.js
+++ models/User.js
@@ -2,10 +2,43 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = Schema({
-  profile: Object
+  profile: Object,
+  email: String
 });
 
+UserSchema.static('findOrCreateByEmail', function(emails, done) {
+  User.find({}).where('email').in(emails).exec(function(err, users) {
+    if (err) { return done(err); }
 
+    var userEmails = users.map(function(user) {
+      return user.email;
+    });
+
+    // create missing users
+    User.create(
+      emails.filter(function(email) {
+        // get emails of missing users
+        return userEmails.indexOf(email) === -1;
+      }).map(function(email) {
+        // convert to object for creation
+        return {email: email};
+      }), 
+
+      function(err) {
+        var args = arguments;
+        var keys = Object.keys(args);
+        keys.shift();
+
+        var newUsers = keys.map(function(key) {
+          return args[key];
+        });
+
+        done(err, users.concat(newUsers)); 
+      }
+    );
+  });
+
+});
 
 mongoose.model('User', UserSchema);
 var User = mongoose.model('User');
