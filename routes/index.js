0694bd4e44ea449434680b443aa4b7a28bc142ed
Zack Michener <zack@zackmichener.net>
2014-12-23 23:58:20 +0000

    add legal page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/legal', function(req, res) {
+  res.render('legal', { title: 'Legal' });
+});
+
 module.exports = router;
