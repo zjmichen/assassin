716df7d7c2181a73719c3fe7d97b6a41cdcebb37
Zack Michener <zack@zackmichener.net>
2014-12-25 21:30:56 +0000

    add route to sign user up for newsletter

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.post('/', function(req, res) {
+  if (!req.accepts('json')) { return res.status(406).end(); }
+  if (!req.body.email) { return res.status(400).end(); }
+
+  var emails = [req.body.email];
+  User.findOrCreateByEmail(emails, function(err, users) {
+    if (err) { return res.status(500).send(err); }
+
+    if (req.query.emailSignup) {
+      var user = users[0];
+      user.receiveNewsletter = true;
+      user.save(function(err) {
+        if (err) { return res.status(500).send(err); }
+        res.end();
+      });
+    } else {
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
