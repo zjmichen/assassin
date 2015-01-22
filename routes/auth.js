37152ef1bf815348d3705e3443a879af02f90194
Zack Michener <zack@zackmichener.net>
2015-01-22 00:06:08 +0000

    change login success redirect

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 router.get('/facebook', passport.authenticate('facebook', {scope: 'email'}));
 router.get('/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/'
+  successRedirect: '/app'
 }));
 
 module.exports = router;
