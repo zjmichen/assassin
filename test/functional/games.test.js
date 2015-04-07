8ce1a740884defd96f579bac918c2a68cebf641c
Zack Michener <zack@zackmichener.net>
2015-04-07 00:41:28 +0000

    remove outdated tests

--- test/functional/games.test.js
+++ test/functional/games.test.js
@@ -74,7 +74,10 @@ describe('/games', function() {
         .send({ players: [users[0]._id, users[1]._id] })
         .expect(200)
         .end(function(err, res) {
-          should.not.exist(err);
+          if (res.error) {
+            throw new Error(res.error.text);
+          }
+
           should.exist(res.body.players);
           res.body.players.length.should.equal(3);
           res.body.players.should.contain(user._id.toString());
@@ -89,85 +92,6 @@ describe('/games', function() {
         .send({ invites: [userEmails[0]] })
         .expect(400)
         .end(done);
-    });
-  });
-
-  describe.skip('old tests', function() {
-    it('should add a player to a game', function(done) {
-      var req = request(app).post('/games/' + game._id + '/players');
-      req.cookies = cookies;
-      req.set('Accept', 'application/json')
-        .expect(200)
-        .end(function(err, res) {
-          should.not.exist(err);
-          should.exist(res.body._id);
-          res.body._id.should.equal(game._id.toString());
-          done();
-        });
-    });
-
-    it('should create invites for a new game', function(done) {
-      var req = request(app).post('/games');
-      req.cookies = cookies;
-      req.set('Accept', 'application/json')
-        .send({
-          invites: [
-            'a@example.com',
-            'b@example.com'
-          ]
-        })
-        .expect(200)
-        .end(function(err, res) {
-          should.not.exist(err);
-          should.exist(res.body._id);
-
-          Invite.find({game: res.body._id}, function(err, invites) {
-            should.not.exist(err);
-            should.exist(invites);
-
-            invites.length.should.equal(2);
-            invites[0].email.should.equal('a@example.com');
-            invites[1].email.should.equal('b@example.com');
-
-            done();
-          });
-        });
-    });
-
-    describe('/assignments', function() {
-      var assignment, target;
-
-      before(function(done) {
-        target = new User();
-        assignment = new Assignment();
-
-        target.save(function(err) {
-          if (err) { console.error(err); }
-          assignment.assassin = user._id;
-          assignment.target = target._id;
-          assignment.game = game._id;
-
-          assignment.save(done);
-        });
-      });
-
-      after(function(done) {
-        Assignment.remove({}, done);
-      });
-
-      it('should list the assignments for a game', function(done) {
-        var req = request(app).get('/games/' + game._id + '/assignments')
-        req.cookies = cookies;
-        req.set('Accept', 'application/json')
-          .expect(200)
-          .end(function(err, res) {
-            should.exist(res.body.gameId);
-            should.exist(res.body.assignments);
-            should.exist(res.body.assignments[0]);
-            res.body.assignments[0]._id.should.equal(assignment._id.toString());
-            done(err);
-          });
-      });
     });
   });
 
